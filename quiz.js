72c2e45c521f71e6b23c5ba6411b8fe753fa7716
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-10-05 10:56:15 +0200

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! done.";
+    }else{
+        document.getElementById("feedback").textContent = "That's wrong. Try again";}
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
